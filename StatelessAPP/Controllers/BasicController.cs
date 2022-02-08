1054d1e42524e8175850a62454b9b24d83e22ace
FiseyskiySV <stanislav.fiseyskiy@akbarsdigital.ru>
2022-02-08 16:41:57 +0300

    First init

---
+++ StatelessAPP/Controllers/BasicController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatelessAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public BasicController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("stateless-operation")]
+        public IActionResult DoStatelessOperation()
+        {
+            var nodeId = _config["NodeId"];
+
+            return Ok(nodeId);
+        }
+    }
+}
---
+++ StatelessAPP/readme.txt
@@ -0,0 +1,6 @@
+https://habr.com/ru/post/648101/
+
+docker-compose up -d
+
+local machine https://localhost:7264/api/Basic/stateless-operation
+http://localhost:5000/api/Basic/stateless-operation
